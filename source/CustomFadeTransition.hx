a453ec37eabb3be4d23497b89e157809bc649f5b
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-19 21:25:47 +0800

    Update CustomFadeTransition.hx

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -36,7 +36,7 @@ class CustomFadeTransition extends MusicBeatSubstate
 		var width:Int = Std.int(FlxG.width);
 		var height:Int = Std.int(FlxG.height);
 
-		transitionSprite = new FlxSprite(width + -1845, height + -1610);
+		transitionSprite = new FlxSprite(width + -923, height + -805);
 		transitionSprite.frames = Paths.getSparrowAtlas('kevin_normal', 'preload');
 		transitionSprite.animation.addByPrefix('transition', 'kevin_normal', 34, false);
 		transitionSprite.scrollFactor.set(0, 0);
